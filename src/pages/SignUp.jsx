d771ac3358178bd2421082b6e631eb130cb568f2
Ayushd1409 <tikendra20040914@gmail.com>
2024-03-16 19:01:03 +0530

    complete remain functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -48,7 +48,7 @@ export default function SignUp() {
   return (
     <section>
       
-        <h1 className='text-3xl text-center font-bold mt-6'>Sign Up</h1>
+        <h1 className='text-3xl text-center font-Times_New_Roman font-semibold mt-6'>Sign Up</h1>
         <div className='flex justify-center flex-wrap items-center py-12 px-6 lg:max-w-6xl mx-auto'>
            <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6' >
             <img className='w-full rounded-2xl' src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?q=80&w=773&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="key" />
